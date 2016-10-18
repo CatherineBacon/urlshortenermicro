752a4dc61c8f91d6fa5494f55adaea3ee0f46bd9
Catherine <catherine.bacon@gmail.com>
2016-10-18 20:05:00 +0100

    add get short url and redirects

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ var port = process.env.PORT || 8080;
 
 var fs = require('fs');
 
+var _ = require('lodash');
+
 // read in library file - use mongodb
 var sites = {};
 
 
 // function to shorten the url
+// needs updating
 var shortenUrl = function(url) {
-    var newUrl = url + '/bum';
+    var newUrl = 'bum';
     return newUrl;
 };
 
@@ -36,5 +39,14 @@ app.put('/new/:url(*)', function(req, res) {
 
 // if url is accessed send user to actual site
 // get and res.redirect()
+app.get('/:shortUrl(*)', function(req, res) {
+    var shortUrl = req.params.shortUrl;
+    var longUrl = _.findKey(sites, function(val) {
+        return val == shortUrl;
+    } );
+    if (longUrl) res.redirect(longUrl);
+    else res.json(sites);
+    //else res.sendStatus(404);
+});
 
 var listener = app.listen(port);
